e759b3ad7f97d3ec6df1e232bb79aee6a61b60af
Pankaj Beniwal <pankajbeniwal3112@gmail.com>
2026-02-09 22:51:28 +0530

    feat(cli): add interactive deploy command with clack prompts

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import { loginCommand } from "./commands/login.js";
+import { deployCommand } from "./commands/deploy.js";
 
 const program = new Command();
 
@@ -13,5 +14,10 @@ program
   .command("login")
   .description("Connect your DigitalOcean account")
   .action(loginCommand);
+
+program
+  .command("deploy [app]")
+  .description("Deploy an application")
+  .action((app?: string) => deployCommand(app));
 
 program.parse();
